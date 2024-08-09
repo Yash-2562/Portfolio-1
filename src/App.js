31150e327e34330610de2f4afd739da08b94fd76
Yash <159153626+Yash-2562@users.noreply.github.com>
2024-08-09 20:56:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Skill from './Components/Skill';
+import About from './Components/About';
+import Extra from './Components/Extra';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer'
+
+
+
+
+
+
+
+function App() {
+    return ( 
+        <div  >
+    
+            <Navbar />
+            <Hero />
+            <Skill />
+            <About />
+            <Extra />
+            <Contact />
+            <Footer />
+            
+           
+        </div>
+    
+) 
+
+}
+
+export default App;
